84d269ef75069c99e7e3a429ea6ce54184a9c19f
Zac <zac.minidude140@Gmail.com>
2023-09-28 08:17:12 -0600

    Updated Header, renamed file, added instructions in comment.

---
+++ Shuffle The Deck/ShuffleTheDeck.vb
@@ -0,0 +1,25 @@
+﻿'Zachary Christensen
+'RCET 2265
+'Fall 2023
+'Shuffle The Deck
+'https://github.com/Minidude140/Shuffle_The_Deck.git
+
+
+Option Explicit On
+Option Strict On
+
+'Write a program that deals a card from a standard deck Of 52 playing cards. 
+'The card should be a random suit And value
+'Suits: spades, clubs, hearts, diamonds
+'Values: 2-10, J, Q, K, A
+'Use a multidimensional array To track If the card has already been dealt. If so draw another random card
+'Shuffle the deck When there are no more cards left Or anytime the user chooses 
+
+
+Module ShuffleTheDeck
+
+    Sub Main()
+
+    End Sub
+
+End Module
